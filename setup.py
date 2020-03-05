9ecfea6e47ddda620588cf910bbaa97994a30218
Addison Schile <addschile@gmail.com>
2020-03-04 18:19:22 -0800

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
-"""pymctdh
+"""pymctdh setup
 """
 import os
 import sys
-from setuptools import setup, Extension
+from setuptools import setup, Extension, find_packages
 from Cython.Build import cythonize
 from Cython.Distutils import build_ext
 
-local_path = os.path.dirname(os.path.abspath(sys.argv[0]))
-os.chdir(local_path)
-sys.path.insert(0, local_path)
-sys.path.insert(0, os.path.join(local_path, 'pymctdh'))  # to retrive _version
+#local_path = os.path.dirname(os.path.abspath(sys.argv[0]))
+#os.chdir(local_path)
+#sys.path.insert(0, local_path)
+#sys.path.insert(0, os.path.join(local_path, 'pymctdh'))  # to retrive _version
 
 ext_modules = [
     Extension("pymctdh.cy.wftools",
@@ -27,30 +27,20 @@ ext_modules = [
 
 # Setup commands go here
 setup(name='pymctdh',
+      python_requires='>=3.6',
+      requires = ['numpy (>=1.12)', 'scipy (>=1.0)', 'cython (>=0.21)'],
       version='0.1',
-      packages=['pymctdh','pymctdh/cy'],
+      packages=find_packages(),
       include_package_data=True,
-      include_dirs = [numpy.get_include(),scipy.get_include()]
       ext_modules = cythonize(EXT_MODULES),
       cmdclass = {'build_ext': build_ext},
       author = 'Addison J. Schile',
       author_email = 'addschile@gmail.com',
       license = 'MIT',
-      description = '',
+      description = 'an open-source MCTDH package written in python',
       long_description = '',
-      keywords = '',
-      classifiers = CLASSIFIERS,
-      platforms = PLATFORMS,
-      requires = ['numpy (>=1.12)', 'scipy (>=1.0)', 'cython (>=0.21)'],
-      package_data = PACKAGE_DATA,
-      install_requires=['numpy>=1.12','scipy>=1.0', 'cython>=0.21'],
+      keywords = 'quantum dynamics, variational principles, tree tensor networks',
+      url = 'https://github.com/addschile/pymctdh'
+      # TODO add classifiers
+      #classifiers = CLASSIFIERS,
 )
-
-outmessage = """\
-################################################################################
-pymctdh is installed and ready to rumble
-if you find this package useful please cite this github page and of course let
-me know!
-################################################################################
-"""
-print(outmessage)
